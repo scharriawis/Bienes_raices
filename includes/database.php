75d4ecdd469bb1c50cd30c415fe0a39c776f7300
scharriawis <scharriawis@gmail.com>
2023-06-09 22:18:04 -0500

    Deployment a la app

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli{
-    $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DB']);
+    $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'] ?? '', $_ENV['DB_DB']);
     $db->set_charset("utf8");
 
 
